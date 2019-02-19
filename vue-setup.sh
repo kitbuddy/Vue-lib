1ed82ee5e036b372ef9c2e9a27b8b738b3e4b271
me.ankit88@gmail.com <me.ankit88@gmail.com>
2019-02-19 00:59:15 -0500

    added git bash file for vue

---
+++ vue-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Ankit Jain Bash file for istalling vue
+
+echo "Welcome file"
+# Install Vue js 
+npm install -g @vue/cli
+
+vue --version
+
+vue create hello-world
+
+echo "Exit Bash file"
+
+
