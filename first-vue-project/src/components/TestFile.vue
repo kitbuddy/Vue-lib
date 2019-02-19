fba5397d14fc992f84d69c4a9c78a1ec58137312
me.ankit88@gmail.com <me.ankit88@gmail.com>
2019-02-19 02:30:17 -0500

    Test Vue project

---
+++ first-vue-project/src/components/TestFile.vue
@@ -0,0 +1,52 @@
+/* eslint-disable */
+<template>
+<div class="hello">
+<h1>{{ msg }}</h1>
+<router-link tag="li" to="/home">
+     <h3><a>HOME PAGE </a> </h3>
+     <h4>{{testName}}</h4>
+</router-link>
+
+<button @click="runClick()">Run</button>
+
+</div>
+</template>
+<script>
+
+
+export default {
+  name: 'TestFile',
+  data () {
+    return {
+      msg: 'Welcome to Test FILE',
+      testName : 'this is Test Name'
+    }
+  },
+  methods:
+   {
+        runClick: () => {
+        alert('Alert')    
+  }
+
+      
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ first-vue-project/src/components/buttonComponent.vue
@@ -0,0 +1,48 @@
+/* eslint-disable */
+<template>
+<div class="hello">
+        <div id="app">
+        <p>{{ foo }}</p>
+        <!-- this will no longer update `foo`! -->
+        <button v-on:click="foo = 'baz'">Change it</button>
+        </div>
+</div>
+</template>
+<script>
+var obj = {
+  foo: 'bar'
+}
+
+Object.freeze(obj)
+
+new Vue({
+  el: '#Button',
+  data: obj
+})
+
+// export default {
+//   name: 'Button',
+//   components: {
+
+//   }
+  
+// }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
